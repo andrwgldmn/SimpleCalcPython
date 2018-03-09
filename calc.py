dabb571f29d4202475938118099598d0ff629716
Andrew <poisk567@gmail.com>
2018-03-09 23:34:56 +0200

    add auto-restart. thanks to Maks4816 for this feature.

--- calc.py
+++ calc.py
@@ -14,81 +14,87 @@ from numpy import *
 from pandas import *
 from matplotlib import *
 
+#Начало
+start = 1
+while start:
+    start = 0
+
 #Запрос на выбор операций
-print ("")
-value = int(input("Выберите нужное действие: \n  \n 1) Сложение  \n 2) Вычитание \n 3) Деление \n 4) Умножение \n 5) Найти синус \n 6) Найти косинус \n 7) Найти тангенс \n 8) Найти котангенс \n 9) Найти производную \n 10) Найти интеграл \n \n  "))
-print ("")
+    print ("")
+    value = int(input("Выберите нужное действие: \n  \n 1) Сложение  \n 2) Вычитание \n 3) Деление \n 4) Умножение \n 5) Найти синус \n 6) Найти косинус \n 7) Найти тангенс \n 8) Найти котангенс \n 9) Найти производную \n 10) Найти интеграл \n \n  "))
+    print ("")
 
-#Сложение
-if (value == 1):
-    x = float(input("Введите первое число: "))
-    print ("")
-    y = float(input("Введите второе число: "))
-    print ("")
-    print ('{} + {}'.format(x, y)) 
-    print ("")    
-    print (x + y)
+    #Сложение
+    if (value == 1):
+        x = float(input("Введите первое число: "))
+        print ("")
+        y = float(input("Введите второе число: "))
+        print ("")
+        print ('{} + {}'.format(x, y)) 
+        print ("")    
+        print (x + y)
 
-#Вычитание
-if (value == 2):
-    x = float(input("Введите первое число: "))
-    print ("")
-    y = float(input("Введите второе число: "))
-    print ("")
-    print ('{} - {}'.format(x, y))   
-    print ("")    
-    print (x - y)
+    #Вычитание
+    if (value == 2):
+        x = float(input("Введите первое число: "))
+        print ("")
+        y = float(input("Введите второе число: "))
+        print ("")
+        print ('{} - {}'.format(x, y))   
+        print ("")    
+        print (x - y)
 
-#Деление
-if (value == 3):
-    x = float(input("Введите первое число: "))
-    print ("")
-    y = float(input("Введите второе число: "))
-    print ("")
-    print ('{} / {}'.format(x, y))
-    print ("")
-    print float(x / y)
+    #Деление
+    if (value == 3):
+        x = float(input("Введите первое число: "))
+        print ("")
+        y = float(input("Введите второе число: "))
+        print ("")
+        print ('{} / {}'.format(x, y))
+        print ("")
+        print float(x / y)
 
-#Умножение
-if (value == 4):
-    x = float(input("Введите первое число: "))
-    print ("")
-    y = float(input("Введите второе число: "))
-    print ("")
-    print ('{} * {}'.format(x, y))
-    print ("")
-    print (x * y)
+    #Умножение
+    if (value == 4):
+        x = float(input("Введите первое число: "))
+        print ("")
+        y = float(input("Введите второе число: "))
+        print ("")
+        print ('{} * {}'.format(x, y))
+        print ("")
+        print (x * y)
 
-#Найти синус
-if (value == 5):
-    x = float(input("Введите число: "))
-    print ("")
-    print float(sin(x))
+    #Найти синус
+    if (value == 5):
+        x = float(input("Введите число: "))
+        print ("")
+        print float(sin(x))
 
-#Найти косинус
-if (value == 6):
-    x = float(input("Введите число: "))
-    print ("")
-    print float(cos(x))
+    #Найти косинус
+    if (value == 6):
+        x = float(input("Введите число: "))
+        print ("")
+        print float(cos(x))
 
-#Найти тангенс
-if (value == 7):
-    x = float(input("Введите число: "))
-    print ("")
-    print float(tan(x))
+    #Найти тангенс
+    if (value == 7):
+        x = float(input("Введите число: "))
+        print ("")
+        print float(tan(x))
 
-#Найти котангенс
-if (value == 8):
-    x = float(input("Введите число: "))
-    tan = tan(x)
-    print ("")
-    print float(1/tan)
-
-#Найти производную:
-if (value == 9):
-    print("Введите выражение: ")
-    print ("")
-    x = Symbol("x")
-    y = y.diff(x)
-    print ("")
-    print ("y")
+    #Найти котангенс
+    if (value == 8):
+        x = float(input("Введите число: "))
+        tan = tan(x)
+        print ("")
+        print float(1/tan)
+#FIXME
+    #Найти производную:
+    if (value == 9):
+        print("Введите выражение: ")
+        print ("")
+        x = Symbol("x")
+        y = y.diff(x)
+        print ("")
+        print ("y")
+    start = 1
